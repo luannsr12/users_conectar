b66c05ed2f07d73f082a3e8032691776d033e049
luannsr12 <luanalvesnsr@gmail.com>
2025-06-19 17:22:49 -0300

    refactor: remove unnecessary comments and clean up .env and vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
+    base: '/',
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(mode), // define apenas o necessário
+    },
     server: {
       port: Number(env.VITE_PORT) || 5173,
-      host: env.VITE_HOST === 'true', // permite acessar via rede local se true
+      host: env.VITE_HOST === 'true',
     },
     preview: {
       port: Number(env.VITE_PORT) || 4173,
